fb726d9dd06d7d995f74fd976bb064f5ce7442a6
Josh Rodriguez <jrodshua@gmail.com>
2021-03-22 16:23:30 -0700

    Delete src/@rocketseat/gatsby-theme-docs/components directory

--- src/@rocketseat/gatsby-theme-docs/components/Logo.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import jrLogo from "../images/josh-logo-transparent.png";
-
-const Logo = () => (
-  <img src={jrLogo} alt="Josh Rodriguez Logo" style={{ width: 90 }} />
-);
-
-export default Logo;
