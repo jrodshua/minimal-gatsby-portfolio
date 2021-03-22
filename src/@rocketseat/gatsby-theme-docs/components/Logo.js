c1d06a62df56712e966687c6043649a6145e7e12
Josh Rodriguez <jrodshua@gmail.com>
2021-03-22 16:22:59 -0700

    Add files via upload

---
+++ src/@rocketseat/gatsby-theme-docs/components/Logo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import jrLogo from "../images/josh-logo-transparent.png";
+
+const Logo = () => (
+  <img src={jrLogo} alt="Josh Rodriguez Logo" style={{ width: 90 }} />
+);
+
+export default Logo;
