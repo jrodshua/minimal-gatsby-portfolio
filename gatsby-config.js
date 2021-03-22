f458b1c85ef8c016658246b07f5cf44e0791ff33
Josh Rodriguez <jrodshua@gmail.com>
2021-03-22 16:02:29 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,52 @@
 module.exports = {
   siteMetadata: {
-    title: `Josh Rodriguez's Portfolio`,
-    description: `Josh Rodriguez's Front End Engineer Portfolio Site`,
-    author: `@jrodshua`,
+    siteTitle: `Rocket Docs`,
+    defaultTitle: `Rocket Docs`,
+    siteTitleShort: `Rocket Docs`,
+    siteDescription: `Out of the box Gatsby Theme for creating documentation websites easily and quickly`,
+    siteUrl: `https://rocketdocs.netlify.app`,
+    siteAuthor: `@rocketseat`,
+    siteImage: `/banner.png`,
+    siteLanguage: `en`,
+    themeColor: `#8257E6`,
+    basePath: `/`,
   },
+  flags: { PRESERVE_WEBPACK_CACHE: true },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: `@rocketseat/gatsby-theme-docs`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        configPath: `src/config`,
+        docsPath: `src/docs`,
+        repositoryUrl: `https://github.com/rocketseat/gatsby-themes`,
+        baseDir: `examples/gatsby-theme-docs`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Josh Rodriguez's Portfolio`,
-        short_name: `Jrodshua Portfolio`,
+        name: `Rocketseat Gatsby Themes`,
+        short_name: `RS Gatsby Themes`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#1A2C42`,
-        display: `minimal-ui`,
-        icon: `src/images/jr-icon.png`, // This path is relative to the root of the site.
+        display: `standalone`,
+        icon: `static/favicon.png`,
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-sitemap`,
+    // {
+    //   resolve: `gatsby-plugin-google-analytics`,
+    //   options: {
+    //     trackingId: `YOUR_ANALYTICS_ID`,
+    //   },
+    // },
+    `gatsby-plugin-remove-trailing-slashes`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://rocketdocs.netlify.com`,
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
-}
+};
